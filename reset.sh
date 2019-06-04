3dacf51663cbe592c34e05e37e99f9de97b9f2a9
zlzforever <zlzforever@163.com>
2019-06-04 16:50:08 +0800

    fix no password issue

--- reset.sh
+++ reset.sh
@@ -2,4 +2,4 @@
 sudo docker stop ids4admin
 sudo docker rm ids4admin
 sudo docker pull zlzforever/ids4admin:latest
-sudo docker run -d --name ids4admin --restart always -e ADMIN_PASSWORD=1qazZAQ! -v ~/ids4admin:/ids4admin -p 6566:6566 zlzforever/ids4admin /ids4admin/appsettings.json
+sudo docker run -d --name ids4admin --restart always -e ADMIN_PASSWORD=1qazZAQ! -v /ids4admin:/ids4admin -p 6566:6566 zlzforever/ids4admin /ids4admin/appsettings.json
--- src/IdentityServer4.Admin/Program.cs
+++ src/IdentityServer4.Admin/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Linq;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
@@ -13,6 +14,11 @@ namespace IdentityServer4.Admin
         public static void Main(string[] args)
         {
             var configFile = args.FirstOrDefault(a => a.Contains("appsettings.json"));
+            if (!File.Exists(configFile))
+            {
+                Console.WriteLine($@"File: {configFile} not exists");
+            }
+
             var mountFolder = configFile != null && File.Exists(configFile)
                 ? Path.GetDirectoryName(configFile)
                 : "";
@@ -34,7 +40,7 @@ namespace IdentityServer4.Admin
                 .WriteTo.Console().WriteTo.RollingFile(logFile,
                     outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}")
                 .CreateLogger();
-
+            Log.Logger.Information($"Arguments: {string.Join(" ", args)}");
             Log.Logger.Information($"Log to: {logFile}");
             var builder = WebHost.CreateDefaultBuilder(args).ConfigureAppConfiguration(config =>
                 {
